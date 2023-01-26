71847478c061e5eb6d7334c1340484c71cf374ab
Gary Smith <garysmith193358@gmail.com>
2023-01-26 16:11:08 -0500

    completed are you playing banjo

---
+++ Codewars/AreYouPlayingBanjo.py
@@ -0,0 +1,9 @@
+def are_you_playing_banjo(name):
+
+    if name[0] == "R" or name[0] == 'r':
+        return f'{name} plays banjo'
+
+    return f'{name} does not play banjo'
+
+    # Time O(1)
+    # Space O(1)
