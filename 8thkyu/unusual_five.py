2c4b66e97a73f1c8f805c0e6c49a8a9b6f6f0f44
Gary Smith <garysmith193358@gmail.com>
2023-05-11 16:12:55 -0400

    added time and space complexity

--- 8thkyu/unusual_five.py
+++ 8thkyu/unusual_five.py
@@ -1,3 +1,6 @@
 def unusual_five():
     # couldnt use digits, so I took a string with the length of 5 and returned the length count
     return len('hello')
+
+#Time: O(1) it runs instantly
+#Space: O(1) takes no space
