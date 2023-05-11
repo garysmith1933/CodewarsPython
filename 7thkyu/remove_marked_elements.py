dd01cec8375d9797728592301f3818f896ced699
Gary Smith <garysmith193358@gmail.com>
2023-05-11 16:04:27 -0400

    added solution explanation

--- 7thkyu/remove_marked_elements.py
+++ 7thkyu/remove_marked_elements.py
@@ -1,7 +1,10 @@
 class List:
     def remove_(self, integer_list, values_list):
+        # set up results variable
         res = []
+        #iterate over list
         for num in integer_list:
+            #only add to results if the number is not in the values list.
             if num not in values_list:
                 res.append(num)
                 
