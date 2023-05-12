1ab031201f75db6ce40aed75b700b0787cd6e246
Gary Smith <garysmith193358@gmail.com>
2023-05-12 12:24:59 -0400

    added explanation

--- 7thkyu/99_problems_last.py
+++ 7thkyu/99_problems_last.py
@@ -1,4 +1,6 @@
 def last(lst):
-    if not lst: return None;
+    # if there is no list. return None
+    if not lst: return None
     
+    # other wise return the last index of the list
     return lst[-1]
